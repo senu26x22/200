c2fb51a0d8487f078233de2a7db039dffc34c310
senu26x22 <rockyxsenu@gmail.com>
2025-04-10 13:49:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ module.exports = {
           },
           logging: false,
         }),
-  PREFIX: (process.env.PREFIX || '^[.,!]').trim(),
+  PREFIX: (process.env.PREFIX || '.').trim(),
   SUDO: process.env.SUDO || '94771665143',
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
   HEROKU_API_KEY: process.env.HEROKU_API_KEY,
